749dcdb3a4320a9d09e587d880f6d5239d39caf2
grizhlieCodes <rafal.potasz@gmail.com>
2025-01-25 06:32:19 +0000

    created embedText for text

---
+++ src/lib/components/ui/text/EmbedText.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import type { TextStoryblok } from './textTypes';
+	import * as ops from './textOptions';
+	import { storyblokEditable, renderRichText, RichTextSchema } from '@storyblok/svelte';
+
+	let { blok }: { blok: any } = $props();
+    // $inspect(blok)
+	const customStoryblokRichTextSchema = {
+		nodes: {
+			...RichTextSchema.nodes,
+			paragraph: () => ({ tag: 'span' }),
+			heading: () => ({ tag: 'span' }),
+			blockquote: () => ({ tag: 'span' })
+		},
+		marks: {
+			...RichTextSchema.marks
+		}
+	};
+
+	const displayOptions = {
+		none: '',
+		inline: 'inline',
+		block: 'block'
+	};
+
+	const renderedContent = $derived(renderRichText(blok.rich_text, { schema: customStoryblokRichTextSchema }));
+	const display = $state(blok.display ? displayOptions[blok.display] : displayOptions.none);
+</script>
+
+<div use:storyblokEditable={blok} class={display}>
+	{@html renderedContent}
+</div>
