12b20a6f6ae8bc9b69ba71a1bca4cf3c231da67f
grizhlieCodes <rafal.potasz@gmail.com>
2025-02-24 19:17:09 +0000

    updated rich text to take in components (wooo)

--- src/lib/components/ui/CustomRichtext.svelte
+++ src/lib/components/ui/CustomRichtext.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
 	import type { CustomRichtextStoryblok } from '$lib/schemas/storyblok/sbTypes';
-	import { renderRichText, storyblokEditable } from '@storyblok/svelte';
+	import { renderRichText, StoryblokComponent, storyblokEditable } from '@storyblok/svelte';
 	let { blok }: { blok: CustomRichtextStoryblok } = $props();
 
 	// $inspect(blok);
 	let headingStyle = blok.heading_styles === 'serif' ? 'prose-headings:font-serif' : 'prose-headings:font-sans';
+
+	function renderNode(node: any) {
+		return renderRichText({
+			type: 'doc',
+			content: [node]
+		});
+	}
+
+	$inspect(blok)
 </script>
 
 <div
@@ -12,5 +21,17 @@
 	class="prose prose-lg prose-headings:font-[500]
 w-full max-w-none {headingStyle} prose-li:!my-1 prose-li:*:!my-0"
 >
-	{@html renderRichText(blok.text)}
+	<!-- {@html renderRichText(blok.text)} -->
+
+	{#if blok?.text?.content}
+		{#each blok?.text.content as node}
+			{#if node?.type === 'blok' && node?.attrs?.body}
+				{#each node?.attrs?.body as blok}
+					<StoryblokComponent {blok}></StoryblokComponent>
+				{/each}
+			{:else}
+				{@html renderNode(node)}
+			{/if}
+		{/each}
+	{/if}
 </div>
