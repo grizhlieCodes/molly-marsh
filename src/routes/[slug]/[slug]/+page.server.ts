09451b880b9b11ee4594bcc9cf4ddbf6cd417a98
grizhlieCodes <rafal.potasz@gmail.com>
2025-02-26 20:36:13 +0000

    added publishing to articles

--- src/routes/[slug]/[slug]/+page.server.ts
+++ src/routes/[slug]/[slug]/+page.server.ts
@@ -9,10 +9,11 @@ const sbJsClient = new StoryblokClient({
 
 export const load: PageServerLoad = async ({ parent, url, params }) => {
 	const slug = url.pathname.slice(1);
+	const version = dev || url.searchParams.has('_storyblok') ? 'draft' : 'published';
 	// let storyblokApi = await useStoryblokApi();
 	const article = await sbJsClient.get(`cdn/stories/${slug}`, {
 		content_type: 'article',
-		version: 'published',
+		version,
 		resolve_relations: ['article.article_tag']
 	});
 
