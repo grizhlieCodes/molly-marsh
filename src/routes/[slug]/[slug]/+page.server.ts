0d0c8b7bd616c0d8db9ca78419d69c7459ce24bb
grizhlieCodes <rafal.potasz@gmail.com>
2025-02-26 20:44:44 +0000

    quick fix

--- src/routes/[slug]/[slug]/+page.server.ts
+++ src/routes/[slug]/[slug]/+page.server.ts
@@ -2,7 +2,7 @@ import type { PageServerLoad } from './$types';
 import { useStoryblokApi } from '@storyblok/svelte';
 import StoryblokClient from 'storyblok-js-client';
 import { PUBLIC_STORYBLOK_ACCESS_TOKEN } from '$env/static/public';
-
+import { dev } from '$app/environment'; // Import the 'dev' flag
 const sbJsClient = new StoryblokClient({
 	accessToken: PUBLIC_STORYBLOK_ACCESS_TOKEN
 });
@@ -10,6 +10,7 @@ const sbJsClient = new StoryblokClient({
 export const load: PageServerLoad = async ({ parent, url, params }) => {
 	const slug = url.pathname.slice(1);
 	const version = dev || url.searchParams.has('_storyblok') ? 'draft' : 'published';
+	console.log(version)
 	// let storyblokApi = await useStoryblokApi();
 	const article = await sbJsClient.get(`cdn/stories/${slug}`, {
 		content_type: 'article',
