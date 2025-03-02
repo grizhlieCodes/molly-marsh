07dc87db0410ab553e1308fddfbe4016132ab559
grizhlieCodes <rafal.potasz@gmail.com>
2025-03-02 08:46:47 +0000

    small update to homepage load

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,10 +1,14 @@
 import type { PageServerLoad } from './$types';
 
+import { dev } from '$app/environment';
+
 export const load: PageServerLoad = async ({ url, params, parent }) => {
 	const { storyblokApi } = await parent();
 
+	const version = dev || url.searchParams.has('_storyblok') ? 'draft' : 'published';
+
 	const dataStory = await storyblokApi.get(`cdn/stories/home`, {
-		version: 'draft',
+		version,
 		resolve_links: 'url'
 	});
 
